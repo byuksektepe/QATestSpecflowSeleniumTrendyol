7fb34e2ab0defffc97503cff64e5517efef95715
Berkantyuks <byuksektepe@hotmail.com>
2022-07-05 17:40:08 +0300

    Common methods and product search elemets

--- QATestSpecflowSeleniumTrendyol/PO/Navbar.cs
+++ QATestSpecflowSeleniumTrendyol/PO/Navbar.cs
@@ -1,24 +1,29 @@
 ﻿using System;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
+using QATestSpecflowSeleniumTrendyol.Resources;
 
 namespace QATestSpecflowSeleniumTrendyol.PO
 {
     public class Navbar
     {
-        private readonly IWebDriver _webDriver;
+        private readonly IWebDriver Driver;
+        private readonly Common common;
 
         public Navbar(IWebDriver webDriver)
         {
-            _webDriver = webDriver;
+            Driver = webDriver;
         }
 
-        private IWebElement SearchInput => _webDriver.FindElement(By.XPath("//div[@class='search-box-container']//input[@class='search-box']"));
-        private IWebElement SearchButton => _webDriver.FindElement(By.XPath("//div[@class='search-box-container']//i[@class='search-icon']"));
+        private IWebElement SearchInput => Driver.FindElement(By.XPath("//div[@class='search-box-container']//input[@class='search-box']"));
+        private IWebElement SearchButton => Driver.FindElement(By.XPath("//div[@class='search-box-container']//i[@class='search-icon']"));
 
         public void SearchItem(String SearchItem)
         {
             SearchInput.SendKeys(SearchItem);
+            SearchInput.SendKeys(Keys.Return);
+            //System.Threading.Thread.Sleep(5000);
+
         }
     }
 }
--- QATestSpecflowSeleniumTrendyol/Resources/Common.cs
+++ QATestSpecflowSeleniumTrendyol/Resources/Common.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace QATestSpecflowSeleniumTrendyol.Resources
 {
     public class Common
     {
         public const string SiteUrl = "https://trendyol.com";
+        public const string ModalCloseElement = "//div[@class='modal-content']//div[@class='modal-close']";
+        public const string PageLoadVerifyElement = "//div[@class='header']//a[@id='logo']";
 
         private readonly IWebDriver _webDriver;
 
@@ -24,6 +31,8 @@ namespace QATestSpecflowSeleniumTrendyol.Resources
             {
                 _webDriver.Manage().Window.Maximize();
                 _webDriver.Url = SiteUrl;
+                CloseModalIfExists();
+
             }
         }
 
@@ -31,6 +40,44 @@ namespace QATestSpecflowSeleniumTrendyol.Resources
         {
             _webDriver.Quit();
         }
+
+        public void CloseModalIfExists()
+        {
+            IWebElement ModalClose = _webDriver.FindElement(By.XPath(ModalCloseElement));
+            if (Exists(ModalClose)) {
+                ModalClose.Click();
+            }
+
+        }
+
+        public static bool Exists(IWebElement element)
+        {
+            if (element == null)
+            { return false; }
+            return true;
+        }
+
+        public void VerifyPageLoad()
+        {
+
+        }
+
+        public void WaitForPageToLoad(Action doing)
+        {
+            IWebElement oldPage = _webDriver.FindElement(By.TagName("html"));
+            doing();
+            WebDriverWait wait = new WebDriverWait(_webDriver, new TimeSpan(0, 0, DefaultWaitInSeconds));
+            try
+            {
+                wait.Until(driver => ExpectedConditions.StalenessOf(oldPage)(_webDriver) &&
+                    ((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState").Equals("complete"));
+            }
+            catch (Exception pageLoadWaitError)
+            {
+                throw new TimeoutException("Timeout during page load", pageLoadWaitError);
+            }
+        }
+
 
         // taken from specflow website, WaitUntil method
         private T WaitUntil<T>(Func<T> getResult, Func<T, bool> isResultAccepted) where T : class
