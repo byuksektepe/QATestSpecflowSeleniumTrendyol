5ea5578d9f79cc246de046876e4075d5ee17d95c
Berkantyuks <byuksektepe@hotmail.com>
2022-07-06 23:50:13 +0300

    Cart page object added

---
+++ QATestSpecflowSeleniumTrendyol/PO/Cart.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using QATestSpecflowSeleniumTrendyol.Resources;
+
+namespace QATestSpecflowSeleniumTrendyol.PO
+{
+    public class Cart
+    {
+    }
+}
