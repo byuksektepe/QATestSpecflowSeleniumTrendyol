4a235d89a1a542ac286da355b097f15c3c9d95b0
Berkantyuks <byuksektepe@hotmail.com>
2022-07-06 22:26:44 +0300

    Product Detail page object added

---
+++ QATestSpecflowSeleniumTrendyol/PO/ProductDetail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using QATestSpecflowSeleniumTrendyol.Resources;
+
+namespace QATestSpecflowSeleniumTrendyol.PO
+{
+    public class ProductDetail
+    {
+
+    }
+}
