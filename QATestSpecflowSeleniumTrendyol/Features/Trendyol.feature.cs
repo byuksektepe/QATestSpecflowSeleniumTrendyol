7587f80d52da395cd7bc63b8a2e19bb43f0de54a
Berkantyuks <byuksektepe@hotmail.com>
2022-07-16 21:04:51 +0300

    SignUp locators and elements added.

--- QATestSpecflowSeleniumTrendyol/Features/Trendyol.feature.cs
+++ QATestSpecflowSeleniumTrendyol/Features/Trendyol.feature.cs
@@ -365,8 +365,8 @@ this.ScenarioInitialize(scenarioInfo);
         [NUnit.Framework.CategoryAttribute("SignInErrors")]
         [NUnit.Framework.TestCaseAttribute("Null email", "", "pass123", "Lütfen geçerli bir e-posta adresi giriniz.", null)]
         [NUnit.Framework.TestCaseAttribute("Invalid email", "wthmail.lolx", "pass123", "Lütfen geçerli bir e-posta adresi giriniz.", null)]
-        [NUnit.Framework.TestCaseAttribute("Null password", "berkant@gmail.com", "", "Lütfen şifrenizi giriniz.", null)]
-        [NUnit.Framework.TestCaseAttribute("Unregistered email", "berkant@gmail.com", "pass123", "E-posta adresiniz ve/veya şifreniz hatalı.", null)]
+        [NUnit.Framework.TestCaseAttribute("Null password", "berkant28@gmail.com", "", "Lütfen şifrenizi giriniz.", null)]
+        [NUnit.Framework.TestCaseAttribute("Unregistered email", "berkant28@gmail.com", "pass123", "E-posta adresiniz ve/veya şifreniz hatalı.", null)]
         public void _07VisitorShouldBeAbleSeeSign_InFormErrorMessages(string description, string email, string password, string expectedMessage, string[] exampleTags)
         {
             string[] @__tags = new string[] {
@@ -410,6 +410,69 @@ this.ScenarioInitialize(scenarioInfo);
 #line hidden
 #line 79
  testRunner.Then(string.Format("Check exception message \"{0}\" in signin form", expectedMessage), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line hidden
+            }
+            this.ScenarioCleanup();
+        }
+        
+        [NUnit.Framework.TestAttribute()]
+        [NUnit.Framework.DescriptionAttribute("08 Visitor should be able see Sign-Up form error messages")]
+        [NUnit.Framework.CategoryAttribute("Smoke")]
+        [NUnit.Framework.CategoryAttribute("SignInErrors")]
+        [NUnit.Framework.TestCaseAttribute("Null email", "", "WhatTheHell!Is4", "E-posta ve şifrenizi giriniz.", "Güçlü Şifre", null)]
+        [NUnit.Framework.TestCaseAttribute("Invalid email", "wthmail.lolx", "WhatTheHell!Is4", "Lütfen geçerli bir email adresi giriniz.", "Güçlü Şifre", null)]
+        [NUnit.Framework.TestCaseAttribute("Null password", "berkant28@gmail.com", "", "E-posta ve şifrenizi giriniz.", "", null)]
+        [NUnit.Framework.TestCaseAttribute("7 < letter pass", "berkant28@gmail.com", "some", "Şifreniz 7 ile 15 karakter arasında olmalıdır.", "Zayıf Şifre", null)]
+        [NUnit.Framework.TestCaseAttribute("7 > letter pass", "berkant28@gmail.com", "somepass", "Şifreniz en az 1 rakam içermelidir.", "Zayıf Şifre", null)]
+        [NUnit.Framework.TestCaseAttribute("7 > letter with numbers pass", "berkant28@gmail.com", "somepass2", "", "Zayıf Şifre", null)]
+        [NUnit.Framework.TestCaseAttribute("Medium password", "berkant28@gmail.com", "somepass2A", "", "Orta Şifre", null)]
+        public void _08VisitorShouldBeAbleSeeSign_UpFormErrorMessages(string description, string email, string password, string expectedMessage, string passwordWarnerMessage, string[] exampleTags)
+        {
+            string[] @__tags = new string[] {
+                    "Smoke",
+                    "SignInErrors"};
+            if ((exampleTags != null))
+            {
+                @__tags = System.Linq.Enumerable.ToArray(System.Linq.Enumerable.Concat(@__tags, exampleTags));
+            }
+            string[] tagsOfScenario = @__tags;
+            System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
+            argumentsOfScenario.Add("Description", description);
+            argumentsOfScenario.Add("Email", email);
+            argumentsOfScenario.Add("Password", password);
+            argumentsOfScenario.Add("ExpectedMessage", expectedMessage);
+            argumentsOfScenario.Add("PasswordWarnerMessage", passwordWarnerMessage);
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("08 Visitor should be able see Sign-Up form error messages", null, tagsOfScenario, argumentsOfScenario, featureTags);
+#line 89
+this.ScenarioInitialize(scenarioInfo);
+#line hidden
+            if ((TagHelper.ContainsIgnoreTag(tagsOfScenario) || TagHelper.ContainsIgnoreTag(featureTags)))
+            {
+                testRunner.SkipScenario();
+            }
+            else
+            {
+                this.ScenarioStart();
+#line 90
+ testRunner.Given("Navigate to trendyol website", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line hidden
+#line 91
+ testRunner.And("Click signup button in navbar", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 92
+ testRunner.And("Verify signup page opened", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 93
+ testRunner.When(string.Format("Set email to \"{0}\" and password to \"{1}\" in signup form", email, password), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line hidden
+#line 94
+ testRunner.And("Click signup button to submit form", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 95
+ testRunner.And(string.Format("Check password warner if required to \"{0}\"", passwordWarnerMessage), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 96
+ testRunner.Then(string.Format("Check exception message \"{0}\" in signup form", expectedMessage), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
             }
             this.ScenarioCleanup();
--- QATestSpecflowSeleniumTrendyol/PO/SignUp.cs
+++ QATestSpecflowSeleniumTrendyol/PO/SignUp.cs
@@ -6,11 +6,28 @@
         private readonly Common common;
         private readonly SubMethods subMethods;
 
+        private const string SignUpVerifyLocator = EmailInputLocator;
+        private const string EmailInputLocator = "//input[@id='register-email']";
+        private const string PasswordInputLocator = "//input[@id='register-password-input']";
+        private const string SignUpButtonLocator = "//button[@class='q-primary q-fluid q-button-medium q-button submit']";
+
+        private const string ExceptionMessageLocator = "//div[@id='error-box-wrapper']//span[@class='message']";
+        private const string PasswordWarnerMessageLocator = "//div[@class='progress-bar-container']/span[contains(@class, 'pw-title')]";
+
+
         public SignUp(IWebDriver driver)
         {
             Driver = driver;
             common = new Common(driver);
             subMethods = new SubMethods(driver);
         }
+
+        private IWebElement SignUpVerifyElement => Driver.FindElement(By.XPath(SignUpVerifyLocator));
+        private IWebElement EmailInputElement => Driver.FindElement(By.XPath(EmailInputLocator));
+        private IWebElement PasswordInputElement => Driver.FindElement(By.XPath(PasswordInputLocator));
+        private IWebElement SignUpButtonElement => Driver.FindElement(By.XPath(SignUpButtonLocator));
+
+        private IWebElement ExceptionMessageElement => Driver.FindElement(By.XPath(ExceptionMessageLocator));
+        private IWebElement PasswordWarnerMessageElement => Driver.FindElement(By.XPath(PasswordWarnerMessageLocator));
     }
 }
